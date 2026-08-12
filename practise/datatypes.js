6630e6e4c1b931964c613d268a55c311cfa7eaae
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-12 15:09:58 +0530

    datatypes(num,string,boolean,float)

---
+++ practise/datatypes.js
@@ -0,0 +1,10 @@
+var num = 123;
+var name = "harsha";
+var Lastname=null;
+var status = true;
+var inactive = false;
+
+console.log("student ID--->",num, "variable type-->",typeof num);
+console.log("studentName--->",name,"variable type-->", typeof name);
+console.log(Lastname,"variable type-->", typeof Lastname)
+console.log("status-->",status,"variable type-->", typeof status);
