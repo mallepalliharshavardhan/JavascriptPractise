ff538b8b62baba4ce9f7a8a99710b7fa7536efd6
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-12 15:42:19 +0530

    Arrays & objects

---
+++ practise/arrays.js
@@ -0,0 +1,67 @@
+// Arrays
+
+// var listID = [1,2,3,4,5,5,6,6,7,7];
+// var studentNames = ["Harsha","Priya", "Idli","veni"]
+
+// console.log(listID)
+// console.log(studentNames)
+
+//--------------------objects------------------//
+
+var marks = [12,345,55,"English","Maths","Science"]
+
+var myMarks ={
+    "telugu":60,
+    "english":80,
+    "physics":90,
+
+}
+var deepikaMarks ={
+    "telugu":61,
+    "english":82,
+    "physics":93,
+
+}
+var madhavaMarks ={
+    "telugu":69,
+    "english":80,
+    "physics":70,
+
+}
+var vasuMarks ={
+    "telugu":60,
+    "english":40,
+    "physics":96,
+
+}
+
+var studentsDetails = {
+    "Harsha":myMarks,
+    "Deepika":deepikaMarks,
+    "Madhava": madhavaMarks,
+    "Vasu": vasuMarks
+}
+
+
+var studentsDetails=[{
+    "telugu":60,
+    "english":80,
+    "physics":90,
+
+},{
+    "telugu":61,
+    "english":82,
+    "physics":93,
+
+},{
+    "telugu":69,
+    "english":80,
+    "physics":70,
+
+},{
+    "Harsha":myMarks,
+    "Deepika":deepikaMarks,
+    "Madhava": madhavaMarks,
+    "Vasu": vasuMarks
+}]
+console.log(studentsDetails)
--- practise/datatypes.js
+++ practise/datatypes.js
@@ -1,3 +1,4 @@
+// --------------datatypes---------// 
 var num = 123;
 var name = "harsha";
 var Lastname=null;
@@ -8,3 +9,15 @@ console.log("student ID--->",num, "variable type-->",typeof num);
 console.log("studentName--->",name,"variable type-->", typeof name);
 console.log(Lastname,"variable type-->", typeof Lastname)
 console.log("status-->",status,"variable type-->", typeof status);
+
+// // types of datatypes //
+
+// 1.primitive types --> Simple,immutable types
+ // 2.non-primitive types--> Arrays,objects, complex structure//
+
+
+
+
+
+
+
