aaf4c5495b1c00be9f8452ecbdf110e86b53a13e
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-13 10:59:29 +0530

    Arrays-push,pop,shift,unshift,lenght

--- practise/arrays.js
+++ practise/arrays.js
@@ -1,67 +1,91 @@
-// Arrays
+// // Arrays
 
-// var listID = [1,2,3,4,5,5,6,6,7,7];
-// var studentNames = ["Harsha","Priya", "Idli","veni"]
+// // var listID = [1,2,3,4,5,5,6,6,7,7];
+// // var studentNames = ["Harsha","Priya", "Idli","veni"]
 
-// console.log(listID)
-// console.log(studentNames)
+// // console.log(listID)
+// // console.log(studentNames)
 
-//--------------------objects------------------//
+// //--------------------objects------------------//
 
-var marks = [12,345,55,"English","Maths","Science"]
+// var marks = [12,345,55,"English","Maths","Science"]
 
-var myMarks ={
-    "telugu":60,
-    "english":80,
-    "physics":90,
+// var myMarks ={
+//     "telugu":60,
+//     "english":80,
+//     "physics":90,
 
-}
-var deepikaMarks ={
-    "telugu":61,
-    "english":82,
-    "physics":93,
+// }
+// var deepikaMarks ={
+//     "telugu":61,
+//     "english":82,
+//     "physics":93,
 
-}
-var madhavaMarks ={
-    "telugu":69,
-    "english":80,
-    "physics":70,
+// }
+// var madhavaMarks ={
+//     "telugu":69,
+//     "english":80,
+//     "physics":70,
 
-}
-var vasuMarks ={
-    "telugu":60,
-    "english":40,
-    "physics":96,
+// }
+// var vasuMarks ={
+//     "telugu":60,
+//     "english":40,
+//     "physics":96,
 
-}
+// }
 
-var studentsDetails = {
-    "Harsha":myMarks,
-    "Deepika":deepikaMarks,
-    "Madhava": madhavaMarks,
-    "Vasu": vasuMarks
-}
+// var studentsDetails = {
+//     "Harsha":myMarks,
+//     "Deepika":deepikaMarks,
+//     "Madhava": madhavaMarks,
+//     "Vasu": vasuMarks
+// }
 
 
-var studentsDetails=[{
-    "telugu":60,
-    "english":80,
-    "physics":90,
+// var studentsDetails=[{
+//     "telugu":60,
+//     "english":80,
+//     "physics":90,
 
-},{
-    "telugu":61,
-    "english":82,
-    "physics":93,
+// },{
+//     "telugu":61,
+//     "english":82,
+//     "physics":93,
 
-},{
-    "telugu":69,
-    "english":80,
-    "physics":70,
+// },{
+//     "telugu":69,
+//     "english":80,
+//     "physics":70,
 
-},{
-    "Harsha":myMarks,
-    "Deepika":deepikaMarks,
-    "Madhava": madhavaMarks,
-    "Vasu": vasuMarks
-}]
-console.log(studentsDetails)
+// },{
+//     "Harsha":myMarks,
+//     "Deepika":deepikaMarks,
+//     "Madhava": madhavaMarks,
+//     "Vasu": vasuMarks
+// }]
+// console.log(studentsDetails)
+
+
+
+var fruitsNames = ["apple","banana"];
+
+// console.log("fruitName-->", fruitsNames);
+// console.log("fruitlist-length->", fruitsNames.length); 
+
+// fruitsNames.push("mango","pinepapple","orange","pomogranete");
+
+
+
+
+// console.log("fruitName-->", fruitsNames);
+// // var name ="harshavardhan";
+// console.log("fruitList-->",fruitsNames.length) 
+
+//  fruitsNames.shift();
+//   fruitsNames.shift();
+
+fruitsNames.unshift();
+
+ console.log("fruitName-->", fruitsNames);
+console.log("fruitList-->",fruitsNames.length);
---
+++ practise/forloop.js
@@ -0,0 +1,9 @@
+var num = 0;
+num++;
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[index];
+    
+}
+
+console.log(num)
