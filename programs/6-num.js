5fd6b9672bd8cd4b3bdc1e438ea9ce4a5d0a8f8f
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-14 17:26:57 +0530

    Positve - negative numbers Program

---
+++ programs/6-num.js
@@ -0,0 +1,39 @@
+//  program to check whether a number is positive or negative
+
+
+var num = 0 ;
+
+if(num > 0){
+    console.log("positive number");
+} else if( num < 0){
+    console.log("negative number");
+}
+else{
+    console.log("number is: 0");
+}
+
+//--------------------positive number -------------
+
+var num = 2257891 ;
+
+if(num > 0){
+    console.log("positive number")
+} else if( num < 0){
+    console.log("negative number")
+}
+else{
+    console.log("number is 0")
+}
+
+//-----------------------------Negative number----------------------
+
+var num = -2257891 ;
+
+if(num > 0){
+    console.log("positive number")
+} else if( num < 0){
+    console.log("negative number")
+}
+else{
+    console.log("number is 0")
+}
