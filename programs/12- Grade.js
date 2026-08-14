579696b4708480211f21cd44503e1783353aa4c9
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-14 20:45:47 +0530

    program to print a grade based on marks

---
+++ programs/12- Grade.js
@@ -0,0 +1,17 @@
+//-----------------  program to print a grade based on marks ---------
+
+var marks = 44;
+
+if(marks < 35 ){
+    console.log("Grade: F");
+}else if(marks >= 35 && marks < 45 ){
+    console.log("Grade: P");
+}else if (marks >= 45 && marks < 65) {
+    console.log("Grade: C");
+}else if (marks >=65 && marks < 75) {
+    console.log("Grade: B");
+}else if (marks >= 75 && marks <= 100 ) {
+    console.log("Grade: A");
+}
+
+/
