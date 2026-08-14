7454337f6d15f6d90bd9f65c2f87a2b12fe21ff0
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-14 20:02:17 +0530

    10- greater in two num's

---
+++ programs/10-greater num.js
@@ -0,0 +1,12 @@
+//-- program to find the greater of two numbers ------
+
+var a= 120;
+var b = 60;
+
+if (a < b){
+    console.log(a,"is less than", b);
+}
+else if(a > b) {
+      console.log(a,"is greater than", b);
+ 
+}
