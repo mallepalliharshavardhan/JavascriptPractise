838480856cc005dbc917b24a70044e721a48e563
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-14 19:54:06 +0530

    9- Marks-pass/fail

---
+++ programs/9-marks.js
@@ -0,0 +1,21 @@
+//------------ program to check whether marks are pass or fail -----------------
+
+var marks = 39;
+
+if( marks <35) {
+     console.log("Marks are-",marks,": Failed ")
+} else{
+    console.log("Marks are-",marks,": Passed ")
+   
+}
+
+//------------------------------------------------------------------------------------
+
+var marks = 34;
+
+if( marks <35) {
+     console.log("Marks are-",marks,": Failed ")
+} else{
+    console.log("Marks are-",marks,": Passed ")
+   
+}
