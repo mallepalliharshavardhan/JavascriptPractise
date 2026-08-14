e43b4a019d2e6f658070d66cd17315d1c0150ab8
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-14 19:46:36 +0530

    8- program for vote eligiblity

---
+++ programs/8-vote.js
@@ -0,0 +1,29 @@
+//-- program to check whether a person is eligible to vote--
+
+var personAge = 10;
+
+if( personAge >= 18){
+    console.log("Person age is -",personAge,"years: Eligible to vote")
+}else {
+    console.log("Person age is -",personAge,"years: Not eligible to vote")
+}
+
+//---------------------------------------------------------------------------------------------
+
+var personAge = 18;
+
+if( personAge >= 18){
+    console.log("Person age is -",personAge,"years: Eligible to vote")
+}else {
+    console.log("Person age is -",personAge,"years: Not eligible to vote")
+}
+
+//--------------------------------------------------------------------------------------------------
+
+var personAge = 26;
+
+if( personAge >= 18){
+    console.log("Person age is -",personAge,"years: Eligible to vote")
+}else {
+    console.log("Person age is -",personAge,"years: Not eligible to vote")
+}
