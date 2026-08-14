872d9a07aaaa83f0c0c6f89d5bc25410a44a9309
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-14 20:16:26 +0530

    11- Password program

---
+++ programs/11- password.js
@@ -0,0 +1,34 @@
+//---  program to check whether a password is correct ----
+
+var password = "123";
+var inputpass = 123 ;
+
+
+if( inputpass == password) {
+    console.log("Password is correct");
+} else {
+    console.log("Incorrect password");
+}
+//---------------------------------------------------------------
+
+var password = "123";
+var inputpass = 123 ;
+
+
+if( inputpass === password) {
+    console.log("Password is correct");
+} else {
+    console.log("Incorrect password");
+}
+
+//---------------------------------------------------------------
+
+var password = "pass123";
+var inputpass = 'pass123' ;
+
+
+if( inputpass === password) {
+    console.log("Password is correct");
+} else {
+    console.log("Incorrect password");
+}
