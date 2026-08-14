4da30d4abd6989aad6e35d0ae06432f4e734f2b3
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-14 19:37:37 +0530

    7- Even -Odd Program

---
+++ programs/7-evenOdd.js
@@ -0,0 +1,20 @@
+// program to check whether a number is even or odd
+
+
+var a = 20;
+
+if( a % 2 == 0){
+    console.log( a +" : is even number");
+}else {
+    console.log( a + " :is odd number");
+}
+
+//-----------------program 2 ----------------------
+
+var a = 15;
+
+if( a % 2 == 0){
+    console.log( a +" : is even number");
+}else {
+    console.log( a + " :is odd number");
+}
