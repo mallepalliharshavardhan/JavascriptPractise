6dd183f228bd8ed7e52701aaeac48bf4cbbd51ba
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 08:55:56 +0530

    program to print numbers from 5 to 1

---
+++ programs/14-loops.js
@@ -0,0 +1,16 @@
+// --- a program to print numbers from 1 to 5 -------------
+
+
+console.log("List of numbers");
+for(i = 1 ; i <= 5; i++){
+    console.log(i);
+}
+
+
+//---- program to print numbers from 5 to 1 -----------------
+
+console.log("List of numbers in reverse");
+for (i = 5 ; i >= 1 ; i--){
+    console.log(i);
+}
+
--- programs/14-nums.js
+++
@@ -1,7 +0,0 @@
-// --- a program to print numbers from 1 to 5 -------------
-
-
-console.log("List of numbers");
-for(i =1 ; i <= 5; i++){
-    console.log(i);
-}
