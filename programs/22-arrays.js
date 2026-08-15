88d67e7214b25ecb58b0c4c8338d29e384b7e8e0
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 20:06:19 +0530

    program to print even numbers from an array

--- programs/22-arrays.js
+++ programs/22-arrays.js
@@ -21,6 +21,14 @@ var nums = [1,2,10,28,3,53,5,8,18];
 
 for (i =0  ; i < nums.length ; i++){
     if (nums[i] > 10){
-        console.log(nums[i]);
+        console.log("numbers greater than 10:",nums[i]);
+    }
+}
+
+// 26. Write a program to print even numbers from an array
+
+for (i=0 ; i < nums.length ; i++){
+    if( nums[i] % 2 == 0){
+        console.log("Even numbers in array",nums[i]);
     }
 }
