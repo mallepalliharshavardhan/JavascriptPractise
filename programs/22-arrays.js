85912fad03d9fe62c8d2c581054255d9e84f5aa5
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 20:01:21 +0530

    program to print numbers greater than 10 from an array

--- programs/22-arrays.js
+++ programs/22-arrays.js
@@ -13,3 +13,14 @@ console.log(li[2]);
 // 24. Write a program to print all elements of an array
 
 console.log(li);
+
+
+//25. Write a program to print numbers greater than 10 from an array
+
+var nums = [1,2,10,28,3,53,5,8,18];
+
+for (i =0  ; i < nums.length ; i++){
+    if (nums[i] > 10){
+        console.log(nums[i]);
+    }
+}
