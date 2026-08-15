979c1349243a4ea4a0a328eee6d89851b774516d
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 20:15:10 +0530

    program to add a fruit to the end of an array using push() --4

---
+++ programs/level7.js
@@ -0,0 +1,9 @@
+// - 27. Write a program to add a fruit to the end of an array using push() --4
+
+var fruitlist = ["Apple","Banana", "Citrus","Dragon"];
+
+console.log(fruitlist);
+
+fruitlist.push("Guva");
+
+console.log(fruitlist);
