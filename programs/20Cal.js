e65dca4c06a84e4fe2605893ddd86b0a4082c2b8
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 19:33:02 +0530

    program to find the sum of numbers from 1 to 5

--- programs/20Cal.js
+++ programs/20Cal.js
@@ -5,3 +5,15 @@
 for ( i =1 ; i <=10 ; i++) {
     console.log( "2 *", i ,"=", 2 * i );
 }
+
+
+//----21. program to find the sum of numbers from 1 to 5 ---------- 0+1= 1, 1+2=3 ,3+3= 6, 6+4=10 ,10+5= 15
+
+
+var sum = 0;
+
+for( i=1 ; i <= 5 ; i++){
+    
+    sum += i;
+}
+console.log("Sum of 1 to 5 number is",sum);
