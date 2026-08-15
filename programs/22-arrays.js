12ef396e4083df80a60d77b627832811c78b2ff8
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 19:47:58 +0530

    program to print the last element of an array

--- programs/22-arrays.js
+++ programs/22-arrays.js
@@ -2,7 +2,10 @@
 
 //  22. Write a program to print the first element of an array
 
-var li = ["first element", " second element"];
+var li = ["first element", " second element", "last element"];
 
 console.log(li[0]);
 
+// 23. Write a program to print the last element of an array
+
+console.log(li[2]);
