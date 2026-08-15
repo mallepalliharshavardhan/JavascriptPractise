be175317af4996fd0f054bdb6f709c8f9dcdc387
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 19:07:11 +0530

    program to print even numbers from 1 to 10

--- programs/14-loops.js
+++ programs/14-loops.js
@@ -1,16 +1,30 @@
-// --- a program to print numbers from 1 to 5 -------------
+// ---14. a program to print numbers from 1 to 5 -------------
 
 
 console.log("List of numbers");
-for(i = 1 ; i <= 5; i++){
+for (i = 1; i <= 5; i++) {
     console.log(i);
 }
 
 
-//---- program to print numbers from 5 to 1 -----------------
+//---- 15.program to print numbers from 5 to 1 -----------------
 
 console.log("List of numbers in reverse");
-for (i = 5 ; i >= 1 ; i--){
+for (i = 5; i >= 1; i--) {
     console.log(i);
 }
+
+// --16.  program to print even numbers from 1 to 10 ----
+
+
+
+    for (i = 0; i <= 10; i++) {
+
+        if (i % 2 === 0){
+
+            console.log(i);
+        }
+     }
+
+
 
