e2dd52cebf8f9f1856867d485e7d9dc975ccd5cb
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 19:37:58 +0530

    program to print the first element of an array

---
+++ programs/22-arrays.js
@@ -0,0 +1,8 @@
+//---------------------------------- Level 6 — Arrays  ---------------------------
+
+//  22. Write a program to print the first element of an array
+
+var li = ["first element", " second element"];
+
+console.log(li[0]);
+
