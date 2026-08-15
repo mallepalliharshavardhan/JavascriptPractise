2e731838941e576cad1bdd0f0c22a58f80ab7dca
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-15 08:41:38 +0530

    program to check whether the temperature is hot or normal

---
+++ programs/13-temp.js
@@ -0,0 +1,16 @@
+// ----  program to check whether the temperature is hot or normal --------
+
+var temp = 45;
+
+if( temp <= 0){
+    console.log("Freezing temperature");
+}
+if ( temp > 0 && temp <= 20){
+    console.log("Cold temperature");
+}
+else if ( temp > 20 && temp <= 28){
+    console.log("Room temperature");
+}
+else if ( temp > 28 ){
+    console.log("Hot temperature");
+}
