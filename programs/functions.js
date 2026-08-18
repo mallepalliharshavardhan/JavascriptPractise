7f0d07c3571a3c334909528d765abef11fcf1233
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-18 19:08:58 +0530

    funsction Subtraction

--- programs/functions.js
+++ programs/functions.js
@@ -4,3 +4,12 @@ function add(a, b){
 }
 let result = add(2 ,9);
 console.log(result);
+
+//----------------------------------------------------------------
+
+function sub(a,b){
+    return a - b ;
+
+}
+let resultSub = sub(14,4);
+console.log(resultSub);
