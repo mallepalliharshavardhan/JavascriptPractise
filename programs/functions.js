95a658c77e1961558f113d846156d7a5e67150a4
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-18 20:03:34 +0530

    functions(sqr, age, +/- num, even/odd)

--- programs/functions.js
+++ programs/functions.js
@@ -13,3 +13,59 @@ function sub(a,b){
 }
 let resultSub = sub(14,4);
 console.log(resultSub);
+
+//-------------------------------------------------------------------------
+
+function sqr(a ,b){
+    return a ** b;
+
+}
+let resultSqr = sqr(2,2);
+console.log("2**2 ->",resultSqr);  
+
+//----------------------------------------------------------------------------
+
+
+function age(thisYear){
+    return thisYear + 1;
+}
+let nextYear = age(26);
+console.log("age next year ->",nextYear);
+
+//----------------------------------------------------------------------------
+
+function numbertype(a){
+    
+    if(a > 0){
+        console.log("Positive num");
+    }
+    else{
+        console.log("Negative num");
+    }
+    return a ;
+}
+
+numbertype(456);
+numbertype(-456);
+
+
+//------------------------------------------------------------------------------
+
+integerType(4);
+
+
+function integerType(i){
+    if(i % 2 === 0){
+        console.log("This is a Even num");
+    }
+    else if( i % 2 !== 0){
+        console.log("This is an Odd num");
+    }
+
+    return i;
+}
+
+integerType(450);
+integerType(45);
+
+//------------------------------------------------------------------
