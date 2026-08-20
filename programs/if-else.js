3a68b6ba6223789bf1b565e06e399f40a79f606a
mallepalliharshavardhan <harsha95055@gmail.com>
2026-08-20 21:40:59 +0530

    nested if else programs

---
+++ programs/if-else.js
@@ -0,0 +1,45 @@
+
+// ................divisble by 2,3,5 ..............
+
+// let num = 30;
+// if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0) 
+// {
+//     if(num % 2 == 0 ){
+//         console.log("2 is the dividor of", num);
+
+//     }
+//      if (num % 3 == 0) {
+//             console.log("3 is the dividor of", num);
+//         } if (num % 5 == 0) {
+//             console.log("5 is the dividor of", num);
+
+//         } 
+//     }
+//     else {
+//         console.log(num, "is not divisible by 2,3 and 5.");
+//     }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>...
+
+// ........ Grades for the marks ................
+
+let marks = 96;
+
+if(marks >= 40){
+     if(marks >= 90){
+        console.log("Grade A");
+    }
+    else if(marks >= 85){
+        console.log("Grade B");
+    }
+    else if(marks >= 75){
+        console.log("Grade C");
+    }
+    else{
+        console.log('Pass');
+    }
+    
+}
+else{
+    console.log('Fail');
+}
